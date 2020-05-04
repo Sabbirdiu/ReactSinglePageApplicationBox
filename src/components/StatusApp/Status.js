d94d892bc03f08de60c4a4945cbd67d589cbf981
Sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-05-04 20:52:50 +0600

    add status app

---
+++ src/components/StatusApp/Status.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export class Status extends Component {
+  state = {
+    status: [
+      {
+        id: 'a',
+        description: 'i am sabbir .i live in keraniganj',
+        time: new Date(),
+      },
+      {
+        id: 'a',
+        description: 'i am sabbir .i live in keraniganj',
+        time: new Date(),
+      },
+    ],
+  };
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Status;
